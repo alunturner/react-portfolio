32fc01ac35b65902204c6c32e54b9e648e88facf
Alun Turner <alun.turner@googlemail.com>
2020-06-25 16:15:33 +0100

    rough separator inserted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Landing from "./components/Landing";
 import About from "./components/About";
+import Separator from "./components/Separator";
 
 function App() {
   return (
     <>
       <Landing />
+      <Separator />
       <About />
+      <Separator />
+      <Separator />
     </>
   );
 }
---
+++ src/components/Separator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Separator() {
+  return <hr />;
+}
+
+export default Separator;
