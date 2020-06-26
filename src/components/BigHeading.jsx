6f0a932347fd12d4ba49a572e1d57b70558db664
Alun Turner <alun.turner@googlemail.com>
2020-06-26 14:06:55 +0100

    added links to the Landing

--- src/components/BigHeading.jsx
+++ src/components/BigHeading.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
 
-function BigHeading({ words }) {
-  return <Typography variant="h1">{words}</Typography>;
+function BigHeading({ word }) {
+  return (
+    <Typography variant="h1">
+      <a href={`#${word}`}>{word}</a>
+    </Typography>
+  );
 }
 
 export default BigHeading;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,9 +4,9 @@ import BigHeading from "./BigHeading";
 function Landing() {
   return (
     <>
-      <BigHeading words={"Alun Turner"} />
-      <BigHeading words={"Portfolio"} />
-      <BigHeading words={"Contact"} />
+      <BigHeading word={"Alun Turner"} />
+      <BigHeading word={"Portfolio"} />
+      <BigHeading word={"Contact"} />
     </>
   );
 }
