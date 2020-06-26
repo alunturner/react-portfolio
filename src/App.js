7051989d0f1a4a92a35e5048461912f0a6433e7e
Alun Turner <alun.turner@googlemail.com>
2020-06-26 11:51:40 +0100

    theme set up complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import { ThemeProvider } from "@material-ui/core";
+import theme from "./resources/theme.jsx";
 
 function App() {
   return (
---
+++ src/resources/theme.jsx
@@ -0,0 +1,71 @@
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+
+let theme = createMuiTheme({
+  palette: {
+    // used to represent primary interface elements for a user
+    primary: {
+      main: "#00008B",
+    },
+    // used to represent secondary interface elements for a user
+    secondary: {
+      main: "#FFFFFF",
+    },
+  },
+
+  typography: {
+    fontFamily: ["Roboto"].join(","),
+    fontSize: 14,
+    h2: {
+      fontSize: 52,
+      "@media screen and (min-width:1024px)": {
+        fontSize: 44,
+      },
+      "@media screen and (min-width:768px)": {
+        fontSize: 36,
+      },
+      "@media screen and (min-width:568px)": {
+        fontSize: 24,
+      },
+      "@media screen and (min-width:320px)": {
+        fontSize: 20,
+      },
+    },
+    h3: {
+      fontSize: 48,
+      "@media screen and (min-width:1024px)": {
+        fontSize: 40,
+      },
+      "@media screen and (min-width:768px)": {
+        fontSize: 32,
+      },
+      "@media screen and (min-width:568px)": {
+        fontSize: 24,
+      },
+      "@media screen and (min-width:320px)": {
+        fontSize: 20,
+      },
+    },
+    h4: {
+      fontSize: 40,
+      "@media screen and (min-width:1024px)": {
+        fontSize: 32,
+      },
+      "@media screen and (min-width:768px)": {
+        fontSize: 24,
+      },
+      "@media screen and (min-width:568px)": {
+        fontSize: 18,
+      },
+      "@media screen and (min-width:320px)": {
+        fontSize: 16,
+      },
+    },
+  },
+
+  // allows us to override css sheets in this file, instructions here https://material-ui.com/customization/globals/
+  overrides: {},
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
