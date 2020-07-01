797b0835d9926a68fd00181db86bb088d62071b0
Alun Turner <alun.turner@googlemail.com>
2020-07-01 11:06:03 +0100

    added links to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,35 +33,43 @@ function Contact() {
             CONTACT
           </Typography>
           <List>
-            <ListItem>
-              <ListItemIcon>
-                <LinkedInIcon />
-              </ListItemIcon>
-              <ListItemText primary="LinkedIn"></ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <EmailIcon />
-              </ListItemIcon>
-              <ListItemText primary="Direct"></ListItemText>
-            </ListItem>
+            <a href="https://linkedin.com/in/alunturner">
+              <ListItem>
+                <ListItemIcon>
+                  <LinkedInIcon />
+                </ListItemIcon>
+                <ListItemText primary="LinkedIn"></ListItemText>
+              </ListItem>
+            </a>
+            <a href="mailto:alun.turner@googlemail.com">
+              <ListItem>
+                <ListItemIcon>
+                  <EmailIcon />
+                </ListItemIcon>
+                <ListItemText primary="Direct"></ListItemText>
+              </ListItem>
+            </a>
           </List>
           <Typography variant="h3" align="center">
             CONNECT
           </Typography>
           <List>
-            <ListItem>
-              <ListItemIcon>
-                <MenuBookIcon />
-              </ListItemIcon>
-              <ListItemText primary="Blog"></ListItemText>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <GitHubIcon />
-              </ListItemIcon>
-              <ListItemText primary="GitHub"></ListItemText>
-            </ListItem>
+            <a href="https://www.flyingtocoding.com/">
+              <ListItem>
+                <ListItemIcon>
+                  <MenuBookIcon />
+                </ListItemIcon>
+                <ListItemText primary="Blog"></ListItemText>
+              </ListItem>
+            </a>
+            <a href="https://github.com/ARTurnerGit">
+              <ListItem>
+                <ListItemIcon>
+                  <GitHubIcon />
+                </ListItemIcon>
+                <ListItemText primary="GitHub"></ListItemText>
+              </ListItem>
+            </a>
           </List>
         </CardContent>
       </Card>
