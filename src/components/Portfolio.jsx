5d3b9a2d2e87ac4e8d7593aa2cc43181bc790f1d
Alun Turner <alun.turner@googlemail.com>
2020-07-01 17:20:23 +0100

    reformat CSS out of portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,21 +3,8 @@ import Tiles from "./Tiles";
 import { Typography, Paper, Card, CardContent } from "@material-ui/core";
 
 function Portfolio() {
-  // geometric is 1600 x 900
-  // let cardWidth = window.innerWidth * 0.75;
-  // let scaleFactor = cardWidth / 1600;
   return (
-    <Paper
-      id="portfolio"
-      elevation={5}
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "10vh 0",
-        backgroundColor: "#a3c1ad",
-      }}
-    >
+    <Paper id="portfolio" elevation={5}>
       <Card className="portfolio-container">
         <CardContent>
           <Typography variant="h4">PORTFOLIO</Typography>
