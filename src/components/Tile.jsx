2eaba5ed0b686eea17c4965ddd058d29225cd562
Alun Turner <alun.turner@googlemail.com>
2020-07-01 11:44:15 +0100

    reformat to add app/repo buttons to each tile

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import picture from "../images/logo192.png";
-import { Typography, Card, CardContent } from "@material-ui/core";
+import {
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+} from "@material-ui/core";
 
 function Tile({ project }) {
   return (
-    <Card
-      className="project-tile"
-      key={project.name}
-      style={{ display: "flex", alignItems: "center" }}
-    >
+    <Card className="project-tile" key={project.name}>
       <img src={picture} alt="placeholder" style={{ width: "20%" }} />
       <CardContent>
         <Typography>{`${project.name}, ${project.description}`}</Typography>
       </CardContent>
+      <CardActions>
+        <Button>Application</Button>
+        <Button>Repository</Button>
+      </CardActions>
     </Card>
   );
 }
