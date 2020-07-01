fc0e3ef41ec0575ed9dc1e9e3c5ab178fe575d74
Alun Turner <alun.turner@googlemail.com>
2020-07-01 10:40:10 +0100

    wrote about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,13 +32,14 @@ function About() {
         <CardContent>
           <Typography variant="h4">ABOUT ME</Typography>
           <Typography variant="body1">
-            My name is Alun Turner and I'm a trainee web developer based in
-            Hampshire in the UK. I was previously a helicopter pilot in the
-            military but now I'm swapping cyclic and collective for keyboard and
-            mouse instead.
-            <br />I love to work with React but I'm constantly seeking out new
-            things to learn and work with. Please have a look at my work and get
-            in touch if you'd like.
+            My name is Alun Turner and I'm a trainee full stack web developer
+            based in Hampshire in the UK. I used to be a helicopter pilot in the
+            military and now I'm swapping cyclic and collective for keyboard and
+            mouse.
+            <br />I primarily write in Javascript and work in React and Node.js.
+            Although these are my primary tools I'm always on the lookout for
+            useful libraries, languages, frameworks and services to learn about
+            and work with to expand my skillset.
           </Typography>
         </CardContent>
       </Card>
