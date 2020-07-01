1f555728bb569e7343f910007143ab47c77ea9f2
Alun Turner <alun.turner@googlemail.com>
2020-07-01 14:36:44 +0100

    added smooth scrolling

--- src/components/Tiles.jsx
+++ src/components/Tiles.jsx
@@ -4,31 +4,31 @@ import Tile from "./Tile";
 function Tiles() {
   let arrayOfProjects = [
     {
-      name: "project1",
+      name: "Tap That Map",
       img: "tap-that-map.png",
       description: "Description of project 1",
       tech: ["tool1", "tool2"],
     },
     {
-      name: "project2",
+      name: "Visualiser App",
       img: "visualiser-frontend.png",
       description: "Description of project 2",
       tech: ["tool1", "tool2"],
     },
     {
-      name: "project3",
+      name: "Visualiser Server",
       img: "visualiser-backend.png",
       description: "Description of project 3",
       tech: ["tool1", "tool2"],
     },
     {
-      name: "project4",
+      name: "NC News App",
       img: "nc-news-frontend.png",
       description: "Description of project 4",
       tech: ["tool1", "tool2"],
     },
     {
-      name: "project5",
+      name: "NC News Server",
       img: "nc-news-backend.png",
       description: "Description of project 5",
       tech: ["tool1", "tool2"],
