16565b7aecb1ef945e954edd6d427fccea45b337
Alun Turner <alun.turner@googlemail.com>
2020-07-02 10:46:38 +0100

    refactor h1 size calc into css

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,19 +2,15 @@ import React from "react";
 import { Paper, Typography } from "@material-ui/core";
 
 function Landing() {
-  let windowWidth = window.innerWidth;
-  let stringLength = "Alun Turner".length;
-  let sizePerChar = Math.floor((1.25 * windowWidth) / stringLength);
-
   return (
     <Paper elevation={5} id="landing">
-      <Typography variant="h1" style={{ fontSize: `${sizePerChar}px` }}>
+      <Typography variant="h1">
         <a href={`#about`}>Alun Turner</a>
       </Typography>
-      <Typography variant="h1" style={{ fontSize: `${sizePerChar}px` }}>
+      <Typography variant="h1">
         <a href={`#portfolio`}>Portfolio</a>
       </Typography>
-      <Typography variant="h1" style={{ fontSize: `${sizePerChar}px` }}>
+      <Typography variant="h1">
         <a href={`#contact`}>Contact</a>
       </Typography>
     </Paper>
